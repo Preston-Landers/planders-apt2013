90706f3fb4d647c91c335276e8419fe311c1282c
Preston Landers <planders@utexas.edu>
2013-08-31 14:24:28 -0500

    Added the Google Account login/logout to Guestbook

--- Guestbook/src/guestbook/GuestbookServlet.java
+++ Guestbook/src/guestbook/GuestbookServlet.java
@@ -1,17 +1,34 @@
 package guestbook;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 public class GuestbookServlet extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7759593256585062849L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, world");
-    }
+			throws IOException {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		if (user != null) {
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+			out.println("<HTML><BODY><P>Hello, " + user.getNickname() + "</P>");
+			out.println("<P><A HREF=\""
+					+ userService.createLogoutURL("/guestbook")
+					+ "\">Logout of Google Account</A></P>");
+			out.println("</BODY></HTML>");
+		} else {
+			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+		}
+	}
 }
