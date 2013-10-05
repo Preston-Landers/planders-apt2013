d9cc419d8f6051549fbc31de39e6091abae76ee4
Preston Landers <planders@utexas.edu>
2013-10-04 20:30:47 -0500

    Got an actual working API call to display results

--- Connexus/ConnexusAndroid/src/main/java/connexus/android/WelcomeActivity.java
+++ Connexus/ConnexusAndroid/src/main/java/connexus/android/WelcomeActivity.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
 import org.joda.time.DateTime;
+
 import com.appspot.connexus_apt.helloworld.Helloworld;
 
 
-public class WelcomeActivity extends Activity {
 
+public class WelcomeActivity extends Activity {
+    private static final String TAG = "WelcomeActivity";
     Helloworld service;
     /**
      * Called when the activity is first created.
@@ -39,12 +45,44 @@ public class WelcomeActivity extends Activity {
         // EditText editText = (EditText) findViewById(R.id.edit_message);
         TextView textView = (TextView) findViewById(R.id.welcome_status_textview);
         DateTime now = new DateTime();
-        textView.setText("yeah, I did it: " + now);
+        textView.setText("starting task at: " + now);
 
-        Helloworld.Builder builder = new Helloworld.Builder(
-                AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null);
-        service = builder.build();
-        service.greetings();
+        new WelcomeTask().execute("hello");
+
     }
+
+
+    private class WelcomeTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            Helloworld.Builder builder = new Helloworld.Builder(
+                    AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null);
+            service = builder.build();
+            String rv = "<No result>";
+            try {
+                String serverMsg = service.greetings().getGreeting(1).execute().getMessage();
+                rv = serverMsg;
+//            } catch (IOException e) {
+//                Log.e(TAG, "hello greeting failed.", e);
+            } catch (Exception e) {
+                Log.e(TAG, "hello greeting failed.", e);
+            }
+            return rv;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.login_progressBar);
+            progressBar.setVisibility(View.INVISIBLE);
+            TextView textView = (TextView) findViewById(R.id.welcome_status_textview);
+            textView.setText("[ " + result + " ] " + new DateTime().now());
+        }
+
+        @Override
+        protected void onPreExecute() {
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.login_progressBar);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
 
