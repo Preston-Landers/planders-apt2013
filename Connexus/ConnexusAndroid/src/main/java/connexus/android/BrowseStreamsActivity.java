d4c99ecf31e56d9d2bda28b3d24cbc93fa0fcb09
Preston Landers <planders@utexas.edu>
2013-10-08 19:43:30 -0500

    Add a couple of icons to the browse action bar.

--- Connexus/ConnexusAndroid/src/main/java/connexus/android/BrowseStreamsActivity.java
+++ Connexus/ConnexusAndroid/src/main/java/connexus/android/BrowseStreamsActivity.java
@@ -80,8 +80,8 @@ public class BrowseStreamsActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.ac_browse_streams_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void loginButton(View view) {
